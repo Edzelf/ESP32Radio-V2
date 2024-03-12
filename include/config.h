cf92390da7497691ee51cc6291393428794ba7d2
Ed Smallenburg <ed@smallenburg.nl>
2024-03-12 13:10:11 +0100

    Add files via upload

--- include/config.h
+++ include/config.h
@@ -18,21 +18,21 @@
   //#define ENABLEOTA                                       // OTA feature
 
   // Define (just one) type of MP3/AAC decoder
-  //#define DEC_VS1053                                      // Hardware decoder for MP3, AAC, OGG
+  #define DEC_VS1053                                        // Hardware decoder for MP3, AAC, OGG
   //#define DEC_VS1003                                      // Hardware decoder for MP3 only
-  #define DEC_HELIX                                         // Software decoder for MP3, AAC. I2S output
+  //#define DEC_HELIX                                       // Software decoder for MP3, AAC. I2S output
   //#define DEC_HELIX_SPDIF                                 // Toslink/Spdif output for MP3, AAC (experimental)
   //#define DEC_HELIX_AI                                    // Software decoder for AI Audio kit (AC101)
   //#define DEC_HELIX_INT                                   // Software decoder for MP3, AAC. DAC output
                                                             // Needs platform = espressif32@3.1.0 !!!!
 
   // Define (just one) type of display.  See documentation.
-  //#define BLUETFT                                         // Works also for RED TFT 128x160
+  #define BLUETFT                                         // Works also for RED TFT 128x160
   //#define ST7789                                          // 240x240 TFT (SPI)
   //#define OLED1306                                        // 64x128 I2C OLED SSD1306
   //#define OLED1309                                        // 64x128 I2C OLED SSD1309
   //#define OLED1106                                        // 64x128 I2C OLED SH1106
-  #define DUMMYTFT                                          // Dummy display
+  //#define DUMMYTFT                                          // Dummy display
   //#define LCD1602I2C                                      // LCD 1602 display with I2C backpack
   //#define LCD2004I2C                                      // LCD 2004 display with I2C backpack
   //#define ILI9341                                         // ILI9341 240*320
@@ -41,8 +41,6 @@
 
   // Define ZIPPYB5 if a ZIPPY B5 Side Switch is used instead of a rotary switch
   //#define ZIPPYB5
-
-  // Define GPIO pins
 
   // End of configuration parameters.
   #define CONFIG_H
